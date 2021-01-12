53d3502a04c41197c4e2f9183a249b3ed8112786
SrinathM <srinathm@Srinath-Air.lan>
2021-01-11 23:05:27 -0500

    Tested nth

--- Tester.java
+++ Tester.java
@@ -1,5 +1,13 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    //Testing nth
+    int number = 31415926;
+    int tenToCol;
+    for (int i = 9; i >= 0; i--) {
+      tenToCol = (int)Math.pow(10, i);
+      int ithDigit = Radix.nth(number, i);
+      System.out.print(ithDigit);
+    }
+    System.out.println();
   }
 }
