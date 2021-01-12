23ffa0f1f64164fdc17fe170a3327a559a0fa8a2
srinathstuycs <smahankali10@stuy.edu>
2021-01-12 00:48:03 -0500

    Added length

--- Radix.java
+++ Radix.java
@@ -1,9 +1,18 @@
 public class Radix {
+
   public static int nth(int n, int col) {
     int tenToCol = (int)Math.pow(10, col + 1);
-    int quotient = n/tenToCol;
-    int remainder = n - quotient*tenToCol;
+    int remainder = Math.abs(n%tenToCol);
     int newnumber = (10 * remainder) / tenToCol;
     return (newnumber%10);
+  }
+
+  public static int length(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    n = Math.abs(n);
+    double log = Math.log10((double)n);
+    return (int)(log + 1);
   }
 }
