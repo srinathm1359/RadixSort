763ba0b264d12203a75a7cfc9f6a17090d167710
srinathstuycs <smahankali10@stuy.edu>
2021-01-12 03:30:09 -0500

    Tested radixSortSimple

--- Tester.java
+++ Tester.java
@@ -16,5 +16,13 @@ public class Tester {
     //Testing length
     System.out.println(Radix.length(positivenumber));
     System.out.println(Radix.length(negativenumber));
+    //Testing radixSortSimple
+    int[] toSort = {176, 338, 147, 299, 120, 43, 1898, 3600, 2, 14, 8, 635};
+    SortableLinkedList sorting = new SortableLinkedList();
+    for (int i = 0; i < toSort.length; i++) {
+      sorting.add(toSort[i]);
+    }
+    System.out.println(sorting);
+    Radix.radixSortSimple(sorting); System.out.println(sorting);
   }
 }
