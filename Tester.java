36fde45f0b4dfd7b566171fa268f18c75bfe5e49
srinathstuycs <smahankali10@stuy.edu>
2021-01-12 00:48:11 -0500

    Tested length

--- Tester.java
+++ Tester.java
@@ -1,13 +1,20 @@
 public class Tester {
   public static void main(String[] args) {
     //Testing nth
-    int number = 31415926;
-    int tenToCol;
+    int positivenumber = 31415926;
     for (int i = 9; i >= 0; i--) {
-      tenToCol = (int)Math.pow(10, i);
-      int ithDigit = Radix.nth(number, i);
+      int ithDigit = Radix.nth(positivenumber, i);
       System.out.print(ithDigit);
     }
     System.out.println();
+    int negativenumber = -1234567;
+    for (int i = 7; i >= 0; i--) {
+      int ithDigit = Radix.nth(negativenumber, i);
+      System.out.print(ithDigit);
+    }
+    System.out.println();
+    //Testing length
+    System.out.println(Radix.length(positivenumber));
+    System.out.println(Radix.length(negativenumber));
   }
 }
