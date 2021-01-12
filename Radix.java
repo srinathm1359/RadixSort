16efc09390061beb4c9cb24bfd8f530f86a448b5
srinathstuycs <smahankali10@stuy.edu>
2021-01-12 03:29:58 -0500

    Added radixSortSimple

--- Radix.java
+++ Radix.java
@@ -24,10 +24,39 @@ public class Radix {
 
   public static void radixSortSimple(SortableLinkedList data) {
     SortableLinkedList[] buckets = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i++) {
+      buckets[i] = new SortableLinkedList();
+    }
+    int biggestLength = 1;
+    //Find largest length and put everything into buckets
+    int currentValue;
+    int currentLength;
+    while (0 < data.size()) {
+      currentValue = data.get(0);
+      currentLength = length(currentValue);
+      //Place newest node's data into bucket for units digit
+      buckets[nth(currentValue, 0)].add(currentValue);
+      //Find largest length
+      if (currentLength > biggestLength) {
+        biggestLength = currentLength;
+      }
+      //avoid duplicating the elements
+      data.remove(0);
+    }
+    merge(data, buckets);
+    //begin the sorting process
+    for (int i = 1; i < biggestLength; i++) {
+      while (0 < data.size()) {
+        currentValue = data.get(0);
+        buckets[nth(currentValue, i)].add(currentValue);
+        data.remove(0);
+      }
+      merge(data, buckets);
+    }
 
   }
 
   public static void radixSort(SortableLinkedList data) {
-    
+
   }
 }
