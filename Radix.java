c8d9e363725541a3d3bafb49654fd75147011e32
SrinathM <srinathm@Srinath-Air.lan>
2021-01-11 23:05:20 -0500

    Added nth

--- Radix.java
+++ Radix.java
@@ -1,4 +1,9 @@
-import java.lang.math;
 public class Radix {
-  
+  public static int nth(int n, int col) {
+    int tenToCol = (int)Math.pow(10, col + 1);
+    int quotient = n/tenToCol;
+    int remainder = n - quotient*tenToCol;
+    int newnumber = (10 * remainder) / tenToCol;
+    return (newnumber%10);
+  }
 }
